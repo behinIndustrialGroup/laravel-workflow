d122130e4b664cbd3c310e58538b90170cd7b672
behinIndustrialGroup <info@behin.energy>
2025-01-06 17:44:10 +0330

    Create fields.php

---
+++ lang/fa/fields.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+  'customer_nid' => 'کدملی مشتری',
+  'customer_mobile' => 'موبایل مشتری',
+  'admision_date' => 'تاریخ پذیرش',
+  'customer_province' => 'استان مشتری',
+  'customer_city' => 'شهر مشتری',
+  'customer_address' => 'آدرس مشتری',
+  'ceo_name' => 'نام مدیرعامل',
+  'device_control_system' => 'سیستم کنترل دستگاه',
+  'customer_suggestions' => 'پیشنهادات مشتری',
+  'customer_init_description' => 'توضیحات اولیه مشتری',
+  'customer_validation_code' => 'کدتایید مشتری',
+  'backup_title' => 'بکاپ گیری',
+  'customer_location' => 'آدرس مشتری',
+  'device information title' => 'اطلاعات دستگاه',
+  'customer_workshop_or_ceo_name' => 'نام مشتری / مدیرعامل / کارگاه',
+);
