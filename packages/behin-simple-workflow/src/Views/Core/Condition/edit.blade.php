c76586ddd4894591e7cb12c814f286ea0afb64f6
behinIndustrialGroup <info@behin.energy>
2025-01-12 20:10:10 +0330

    Update edit.blade.php

--- packages/behin-simple-workflow/src/Views/Core/Condition/edit.blade.php
+++ packages/behin-simple-workflow/src/Views/Core/Condition/edit.blade.php
@@ -5,33 +5,66 @@
 @endphp
 
 @section('content')
-    <h1>Edit Script</h1>
-    <div class="container">
+    <h1>{{ trans('fields.Edit Condition') }}</h1>
+    <div class="container p-4 border rounded shadow-sm bg-light">
         <form action="{{ route('simpleWorkflow.conditions.update', $condition->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <table class="table">
+            <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th>{{ trans('Id') }}</th>
-                        <th>{{ trans('Field Name') }}</th>
-                        <th>{{ trans('Operation') }}</th>
-                        <th>{{ trans('Value') }}</th>
-                        <th>{{ trans('Task') }}</th>
+                        <th>{{ trans('fields.Id') }}</th>
+                        <th>{{ trans('fields.Field Name') }}</th>
+                        <th>{{ trans('fields.Operation') }}</th>
+                        <th>{{ trans('fields.Value') }}</th>
+                        <th>{{ trans('fields.Task') }}</th>
                         <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     @if (is_array($content))
-                        @foreach ($content as $field)
+                        @foreach ($content as $row)
                             <tr>
-                                <td></td>
-                                <td><input type="text" name="fieldName[{{ $index }}]"
-                                        value="{{ $field->fieldName }}"></td>
-                                <td><input type="text" name="operation[{{ $index }}]" value="{{ $field->operation }}"></td>
-                                <td><input type="text" name="value[{{ $index }}]" value="{{ $field->value }}"></td>
-                                <td><input type="text" name="task[{{ $index }}]" value="{{ $field->task }}"></td>
-                                <td></td>
+                                <td>{{ $index + 1 }}</td>
+                                <td><input type="text" name="fieldName[{{ $index }}]" class="form-control"
+                                        value="{{ $row->fieldName }}" list="fields">
+                                    <datalist id="fields">
+                                        @foreach (getProcessFields() as $field)
+                                            <option value="{{ $field->name }}"
+                                                {{ $field->name == $row->fieldName ? 'selected' : '' }}>
+                                                ({{ $field->type }})
+                                                {{ $field->name }} {{ trans('fields.' . $field->name) }}</option>
+                                        @endforeach
+                                    </datalist>
+                                </td>
+                                <td>
+                                    <select name="operation[{{ $index }}]" id="" class="form-control">
+                                        <option value="=" {{ $row->operation == '=' ? 'selected' : '' }}>=</option>
+                                        <option value=">" {{ $row->operation == '>' ? 'selected' : '' }}>></option>
+                                        <option value="<" {{ $row->operation == '<' ? 'selected' : '' }}>
+                                            << /option>
+                                        <option value=">=" {{ $row->operation == '>=' ? 'selected' : '' }}>>=
+                                        </option>
+                                        <option value="<=" {{ $row->operation == '<=' ? 'selected' : '' }}>
+                                            <=< /option>
+                                    </select>
+                                </td>
+                                <td><input type="text" name="value[{{ $index }}]" class="form-control"
+                                        value="{{ $row->value }}"></td>
+                                <td><input type="text" name="task[{{ $index }}]" class="form-control"
+                                        value="{{ $row->task }}" list="tasks">
+                                    <datalist id="tasks">
+                                        @foreach (getProcessTasks() as $task)
+                                            <option value="{{ $task->id }}"
+                                                {{ $task->id == $row->task ? 'selected' : '' }}>{{ $task->name }}
+                                            </option>
+                                        @endforeach
+                                    </datalist>
+                                </td>
+                                <td>
+                                    <button class="btn btn-danger" type="button" onclick="removeTr(this)"><i
+                                            class="fa fa-trash"></i></button>
+                                </td>
                             </tr>
                             @php
                                 $index++;
@@ -41,19 +74,48 @@
 
                 </tbody>
                 <tfoot>
-                    {{-- <input type="hidden" name="formId" value="{{ $condition->id }}"> --}}
                     <tr>
                         <td></td>
-                        <td><input type="text" name="fieldName[{{ $index }}]" id="" value=""></td>
-                        <td><input type="text" name="operation[{{ $index }}]" id="" value="">
-                        <td><input type="text" name="value[{{ $index }}]" id="" value="">
-                        <td><input type="text" name="task[{{ $index }}]" id="" value="">
+                        <td><input type="text" name="fieldName[{{ $index }}]" class="form-control"
+                                value="" list="fields">
+                            <datalist id="fields">
+                                @foreach (getProcessFields() as $field)
+                                    <option value="{{ $field->name }}">
+                                        ({{ $field->type }})
+                                        {{ $field->name }} {{ trans('fields.' . $field->name) }}</option>
+                                @endforeach
+                            </datalist>
                         </td>
-                        <td><button class="btn btn-success">{{ trans('Edit') }}</button></td>
+                        <td>
+                            <select name="operation[{{ $index }}]" id="" class="form-control">
+                                <option value="=" >=</option>
+                                <option value=">" >></option>
+                                <option value="<" ><</option>
+                                <option value=">=" >>=</option>
+                                <option value="<=" ><=</option>
+                            </select>
+                        </td>
+                        <td><input type="text" name="value[{{ $index }}]" class="form-control" id=""
+                                value="">
+                        </td>
+                        <td><input type="text" name="task[{{ $index }}]" class="form-control" id=""
+                                value="" list="tasks">
+                            <datalist id="tasks">
+                                @foreach (getProcessTasks() as $task)
+                                    <option value="{{ $task->id }}">{{ $task->name }}</option>
+                                @endforeach
+                            </datalist>
+                        </td>
+                        <td><button class="btn btn-success" type="submit">{{ trans('Edit') }}</button></td>
                     </tr>
                 </tfoot>
             </table>
         </form>
 
     </div>
+    <script>
+        function removeTr(element) {
+            element.parentNode.parentNode.remove();
+        }
+    </script>
 @endsection
