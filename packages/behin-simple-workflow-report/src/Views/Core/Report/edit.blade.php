26dd58199c35c334e621ed81c18480bdfde2b6dc
behinIndustrialGroup <info@behin.energy>
2025-01-15 15:46:09 +0330

    Update edit.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/Report/edit.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/Report/edit.blade.php
@@ -72,10 +72,32 @@
             </div>
         </div>
     </div>
+    <div class="d-flex justify-content-end bg-white p-2 mt-2">
+        <button class="btn btn-sm btn-outline-primary m-1" onclick="saveForm()">
+            <i class="fa fa-save"></i> {{ trans('fields.Save') }}
+        </button>
+    </div>
 @endsection
 
 @section('script')
     <script>
         initial_view();
     </script>
+    function saveForm() {
+        var form = $('#form')[0];
+        var fd = new FormData(form);
+        send_ajax_formdata_request(
+            '{{ route('simpleWorkflow.routing.save') }}',
+            fd,
+            function(response) {
+                console.log(response);
+                if (response.status == 200) {
+                    show_message(response.msg)
+                    window.location.reload();
+                } else {
+                    show_error(response.msg);
+                }
+            }
+        )
+    }
 @endsection
