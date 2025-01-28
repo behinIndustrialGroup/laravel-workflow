0108555125eeb0ff66c80539b6c0113edff95081
behinIndustrialGroup <info@behin.energy>
2025-01-28 20:23:20 +0330

    Update SendSmsToCustomer.php

--- packages/behin-simple-workflow/src/Controllers/Scripts/SendSmsToCustomer.php
+++ packages/behin-simple-workflow/src/Controllers/Scripts/SendSmsToCustomer.php
@@ -7,13 +7,15 @@ use Behin\SimpleWorkflow\Controllers\Core\VariableController;
 use Behin\SimpleWorkflow\Models\Core\Process;
 use Behin\SimpleWorkflow\Models\Core\Task;
 use Behin\SimpleWorkflow\Models\Core\Variable;
+use Behin\Sms\Controllers\SmsController;
 use Illuminate\Http\Request;
 
 class SendSmsToCustomer extends Controller
 {
     protected $case;
-    public function __construct($case) {
-        // $this->case = $case;
+    public function __construct($case)
+    {
+        $this->case = $case;
         // return VariableController::save(
         //     $this->case->process_id, $this->case->id, 'manager', 2
         // );
@@ -21,8 +23,15 @@ class SendSmsToCustomer extends Controller
 
     public function execute()
     {
-
-
+        $caseNumber = $this->case->number;
+        $variables = $this->case->variables();
+        $customerMobile = $variables->where('key', 'customer_mobile')->first()?->value;
+        if ($customerMobile) {
+            $params = [
+                "name" => "CASENUMBER",
+                "value" => $caseNumber
+            ];
+            $result = SmsController::sendByTemp($customerMobile, 844292, $params);
+        }
     }
-
 }
