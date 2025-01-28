3bd646fc1884b66c74de57a2fd11d69a82c73ba9
behinIndustrialGroup <info@behin.energy>
2025-01-28 18:26:11 +0330

    ارسال پیامک به سرپرست تعمیرات

--- packages/behin-sms/src/Controllers/SmsController.php
+++ packages/behin-sms/src/Controllers/SmsController.php
@@ -16,28 +16,74 @@ class SmsController extends Controller
     public function __construct() {
         
     }
-    public static function send($to, $text)
+    public static function send($to, $code)
     {
-        $username = '09376265059';
-        $password = '1566bf7b-70fb-4e41-a635-0ea0612a7d28';
-        $api = new MelipayamakApi($username,$password);
-        $smsRest = $api->sms();
-        $to = '09376922176';
-        $from = '50004001265059';
-        $response = $smsRest->send($to, $from, $text, false);
-        $json = json_decode($response);
-        return $json;
+        $curl = curl_init();
+        $postFields = array(
+            "mobile" => $to,
+            "templateId" => 187709,
+            "parameters" => array([
+                "name" => "CODE",
+                "value" => $code
+            ])
+        );
+        $postFields = json_encode($postFields);
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.sms.ir/v1/send/verify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postFields,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Accept: text/plain',
+                'x-api-key: '.env('SMS_IR_API_KEY')
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return json_decode($response);
     }
 
-    public function sendByBaseNumber($to, $bodyId, array $text)
+    public static function sendByTemp($to, $tempCode, array $parameter)
     {
-        $username = '09376265059';
-        $password = '1566bf7b-70fb-4e41-a635-0ea0612a7d28';
-        $api = new MelipayamakApi($username,$password);
-        $smsRest = $api->sms();
-        $response = $smsRest->sendByBaseNumber($text, $to, $bodyId);
-        $json = json_decode($response);
-        return $json;
+        $curl = curl_init();
+        $postFields = array(
+            "mobile" => $to,
+            "templateId" => $tempCode,
+            "parameters" => array($parameter)
+        );
+        $postFields = json_encode($postFields);
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.sms.ir/v1/send/verify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postFields,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Accept: text/plain',
+                'x-api-key: '.env('SMS_IR_API_KEY')
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return json_decode($response);
     }
+
 
 }
