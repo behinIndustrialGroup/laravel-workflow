34bc059a78a8369f2ae6a3cf1c7df91b452f2b4e
behinIndustrialGroup <info@behin.energy>
2025-01-30 11:04:42 +0330

    Update ChangeLastStatus.php

--- packages/behin-simple-workflow/src/Controllers/Scripts/ChangeLastStatus.php
+++ packages/behin-simple-workflow/src/Controllers/Scripts/ChangeLastStatus.php
@@ -7,22 +7,27 @@ use Behin\SimpleWorkflow\Controllers\Core\VariableController;
 use Behin\SimpleWorkflow\Models\Core\Process;
 use Behin\SimpleWorkflow\Models\Core\Task;
 use Behin\SimpleWorkflow\Models\Core\Variable;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ChangeLastStatus extends Controller
 {
     protected $case;
-    public function __construct($case) {
+    public function __construct($case)
+    {
         $this->case = $case;
-        
     }
 
     public function execute()
     {
-        // $last_status = $this->case->variables()->where('key', 'last_status')->frist()->value;
-        // VariableController::save(
-        //     $this->case->process_id, $this->case->id, 'last_status', $last_status
-        // );
-    }
+        $today = Carbon::today();
 
+        $records = DB::table('wf_cases')
+            ->select('process_id', DB::raw('COUNT(*) as record_count'))
+            ->whereDate('created_at', $today)
+            ->groupBy('process_id')
+            ->first();
+        return $records->record_count;
+    }
 }
