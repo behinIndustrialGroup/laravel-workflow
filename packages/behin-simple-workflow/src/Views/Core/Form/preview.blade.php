40577ecb4c1eed9a31192a5327c8050f8827d618
behinIndustrialGroup <info@behin.energy>
2025-02-06 18:59:36 +0330

    Update preview.blade.php

--- packages/behin-simple-workflow/src/Views/Core/Form/preview.blade.php
+++ packages/behin-simple-workflow/src/Views/Core/Form/preview.blade.php
@@ -4,7 +4,7 @@
     $content = collect($content)->sortBy('order')->toArray();
 @endphp
 <script src="{{ url('packages/behin-form-builder/src/js/signature_pad.umd.min.js') }}"></script>
-<div class="row">
+<div class="row col-sm-12 p-0 m-0">
     @foreach ($content as $field)
         @php
             $fieldLabel = trans('SimpleWorkflowLang::fields.' . $field->fieldName);
