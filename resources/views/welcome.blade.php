9c1818c90ce7be8975de2956ecfc6c70f6c1a5ab
behinIndustrialGroup <info@behin.energy>
2025-02-18 16:51:13 +0330

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,13 @@
 @extends('behin-layouts.welcome')
 
 @section('content')
-<div class="register-box">
-    <div class="card card-outline card-primary">
-        <div class="card-header text-center">
-            <img src="https://altfuel.ir/fa/public/logo.png" class="col-sm-12" alt="">
-        </div>
-        <div class="card-body">
-            <form action="javascript:void(0)" method="post" id="login-form">
-                @csrf
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" name="email" placeholder="موبایل">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fa fa-phone"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" placeholder="رمز عبور">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fa fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary col-sm-12" onclick="submit()">ورود</button>
-            </div>
-            <hr>
-            <div class="center-align" style="text-align: center">
-                <a href="{{ route('register') }}" class="text-center">صفحه ثبت نام</a>
-            </div>
-            <hr>
-            <div class="center-align" style="text-align: center">
-                <a href="{{ route('password.request') }}" class="text-center">فراموشی رمز</a>
-            </div>
-        </div>
+@endsection
 
-    </div>
-</div>
+@section('script')
+    <script>
+        $.get('https://survey.porsline.ir/file/b74ab4fe6d9f882c-x0chO1h1/359859538/32368695-034-امضا.png/download?hli=1', function(response){
+            return response;
+        })
+    </script>
 @endsection
