ef39a4b9abc13b8743d833e396a00eb125d3909d
behinIndustrialGroup <info@behin.energy>
2025-03-03 10:16:46 +0330

    Update 211ed341-c06c-41cb-881c-d33e8d4cd905.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/Summary/process/211ed341-c06c-41cb-881c-d33e8d4cd905.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/Summary/process/211ed341-c06c-41cb-881c-d33e8d4cd905.blade.php
@@ -46,8 +46,8 @@
                                             <th>ماه</th>
                                             <th>مجموع تایید شده</th>
                                             <th>تایید نشده / در انتظار تایید</th>
-                                            <th>مجموع مرخصی (ساعت)</th>
-                                            <th>مانده مرخصی (ساعت)</th>
+                                            <th>مجموع مرخصی</th>
+                                            <th>مانده مرخصی</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -71,24 +71,21 @@
                 @endif
 
                 <div class="card">
-                    <div class="card-header text-center bg-warning">لیست پرونده های فرآیند {{ $process->name }}</div>
+                    <div class="card-header text-center bg-warning">جدول مرخصی های ساعتی</div>
 
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-bordered" id="draft-list">
+                            {{-- جدول مرخصی‌های ساعتی --}}
+                            <table class="table table-bordered" id="hourly-leaves">
                                 <thead>
                                     <tr>
-                                        {{-- <th>ردیف</th> --}}
                                         <th class="d-none">شناسه</th>
                                         <th>شماره پرونده</th>
                                         <th>ایجاد کننده</th>
                                         <th>نوع مرخصی</th>
-                                        <th>تاریخ شروع(مخصوص مرخصی های ساعتی)</th>
-                                        <th>ساعت شروع(مخصوص مرخصی های ساعتی)</th>
-                                        <th>ساعت پایان(مخصوص مرخصی های ساعتی)</th>
-                                        <th>تاریخ شروع(برای مرخصی های روزانه)</th>
-                                        <th>تاریخ پایان(برای مرخصی های روزانه)</th>
-                                        <th>مدت مرخصی (برای مرخصی های روزانه)</th>
+                                        <th>تاریخ شروع</th>
+                                        <th>ساعت شروع</th>
+                                        <th>ساعت پایان</th>
                                         <th>مدیر دپارتمان</th>
                                         <th>تایید مدیر دپارتمان</th>
                                         <th>اقدام</th>
@@ -96,25 +93,72 @@
                                 </thead>
                                 <tbody>
                                     @foreach ($process->cases as $case)
-                                        @if ($case->getVariable('department_manager'))
+                                        @if ($case->getVariable('timeoff_request_type') === 'ساعتی' && $case->getVariable('department_manager') && $case->getVariable('user_department_manager_approval'))
                                             <tr>
-                                                {{-- <td>{{ $loop->iteration }}</td> --}}
                                                 <td class="d-none">{{ $case->id }}</td>
                                                 <td>{{ $case->number }}</td>
                                                 <td>{{ $case->creator()?->name }}</td>
-
                                                 <td>{{ $case->getVariable('timeoff_request_type') }}</td>
+                                                <td>{{ $case->getVariable('timeoff_start_date') }}</td>
                                                 <td>{{ $case->getVariable('timeoff_start_time') }}</td>
                                                 <td>{{ $case->getVariable('timeoff_end_time') }}</td>
-                                                <td>{{ $case->getVariable('timeoff_start_date') }}</td>
+                                                <td>{{ getUserInfo($case->getVariable('department_manager'))?->name }}</td>
+                                                <td>{{ $case->getVariable('user_department_manager_approval') }}</td>
+                                                <td>
+                                                    <a
+                                                        href="{{ route('simpleWorkflowReport.summary-report.edit', ['summary_report' => $case->id]) }}">
+                                                        <button
+                                                            class="btn btn-primary btn-sm">{{ trans('fields.Show More') }}</button>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header text-center bg-warning">جدول مرخصی های روزانه</div>
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            {{-- جدول مرخصی‌های روزانه --}}
+                            <table class="table table-bordered" id="daily-leaves">
+                                <thead>
+                                    <tr>
+                                        <th class="d-none">شناسه</th>
+                                        <th>شماره پرونده</th>
+                                        <th>ایجاد کننده</th>
+                                        <th>نوع مرخصی</th>
+                                        <th>تاریخ شروع</th>
+                                        <th>تاریخ پایان</th>
+                                        <th>مدت مرخصی</th>
+                                        <th>مدیر دپارتمان</th>
+                                        <th>تایید مدیر دپارتمان</th>
+                                        <th>اقدام</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($process->cases as $case)
+                                        @if ($case->getVariable('timeoff_request_type') === 'روزانه' && $case->getVariable('department_manager') && $case->getVariable('user_department_manager_approval'))
+                                            <tr>
+                                                <td class="d-none">{{ $case->id }}</td>
+                                                <td>{{ $case->number }}</td>
+                                                <td>{{ $case->creator()?->name }}</td>
+                                                <td>{{ $case->getVariable('timeoff_request_type') }}</td>
                                                 <td>{{ $case->getVariable('timeoff_start_date') }}</td>
                                                 <td>{{ $case->getVariable('timeoff_end_date') }}</td>
                                                 <td>{{ $case->getVariable('timeoff_daily_request_duration') }}</td>
                                                 <td>{{ getUserInfo($case->getVariable('department_manager'))?->name }}</td>
                                                 <td>{{ $case->getVariable('user_department_manager_approval') }}</td>
-                                                <td><a
-                                                        href="{{ route('simpleWorkflowReport.summary-report.edit', ['summary_report' => $case->id]) }}"><button
-                                                            class="btn btn-primary btn-sm">{{ trans('fields.Show More') }}</button></a>
+                                                <td>
+                                                    <a
+                                                        href="{{ route('simpleWorkflowReport.summary-report.edit', ['summary_report' => $case->id]) }}">
+                                                        <button
+                                                            class="btn btn-primary btn-sm">{{ trans('fields.Show More') }}</button>
+                                                    </a>
                                                 </td>
                                             </tr>
                                         @endif
