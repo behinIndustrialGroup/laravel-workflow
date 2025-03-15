a6d5691381d1642136435136a99e8bcf11e8b876
behinIndustrialGroup <info@behin.energy>
2025-03-15 10:01:53 +0330

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -4,6 +4,8 @@ return [
     'default' => env('BROADCAST_DRIVER', 'pusher'),
     'pusher' => [
         'driver' => 'pusher',
+        'instanceId' => env('PUSHER_INSTANCE_ID'),
+        'secretKey' => env('PUSHER_SECRET_KEY'),
         'key' => env('PUSHER_APP_KEY'),
         'secret' => env('PUSHER_APP_SECRET'),
         'app_id' => env('PUSHER_APP_ID'),
