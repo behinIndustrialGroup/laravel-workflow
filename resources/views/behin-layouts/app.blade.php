98890ec95bed87b95e5626cdc1b0b79950788040
behinIndustrialGroup <info@behin.energy>
2025-03-27 15:32:48 +0330

    icon

--- resources/views/behin-layouts/app.blade.php
+++ resources/views/behin-layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ url('public/behin/logo.ico') . '?' . config('app.version') }}">
 
     <title>@yield('title')</title>
     <!-- Tell the browser to be responsive to screen width -->
