efd52ab1f54712b9924273750546decf83904f82
behinIndustrialGroup <info@behin.energy>
2025-03-30 01:22:56 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Behin\SimpleWorkflow\Controllers\Core\PushNotifications;
 use BehinInit\App\Http\Middleware\Access;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Mkhodroo\AgencyInfo\Controllers\GetAgencyController;
 use Pusher\Pusher;
@@ -35,10 +36,41 @@ Route::get('/pusher/beams-auth', function (Request $request) {
     $beamsToken = $beamsClient->generateToken('user-mobile-'.$userId);
     // $user = User::find($userId);
     return response()->json($beamsToken);
-    return response()->json([
-        'token' => $beamsToken
-    ]);
 })->middleware('auth');
+
+Route::get('queue-work', function () {
+    $limit = 5; // تعداد jobهای پردازش شده در هر درخواست
+    $jobs = DB::table('jobs')->orderBy('id')->limit($limit)->get();
+
+    foreach ($jobs as $job) {
+        try {
+            // دیکد کردن محتوای job
+            $payload = json_decode($job->payload, true);
+            $command = unserialize($payload['data']['command']);
+
+            // اجرای job
+            $command->handle();
+
+            // حذف job پس از اجرا
+            DB::table('jobs')->where('id', $job->id)->delete();
+
+            // return 'Job processed: ' . $job->id;
+        } catch (Exception $e) {
+            // در صورت خطا، job را به جدول failed_jobs منتقل کنید
+            DB::table('failed_jobs')->insert([
+                'connection' => $job->connection,
+                'queue' => $job->queue,
+                'payload' => $job->payload,
+                'exception' => (string) $e,
+                'failed_at' => now()
+            ]);
+
+            DB::table('jobs')->where('id', $job->id)->delete();
+
+            return 'Job failed: ' . $e->getMessage();
+        }
+    }
+});
 
 Route::get('build-app', function(){
     Artisan::call('cache:clear');
