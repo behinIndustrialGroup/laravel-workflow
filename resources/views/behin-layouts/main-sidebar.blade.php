428a0608f832a1a10ae327febad6bf507ef696aa
behinIndustrialGroup <info@behin.energy>
2025-03-30 02:23:13 +0330

    ui

--- resources/views/behin-layouts/main-sidebar.blade.php
+++ resources/views/behin-layouts/main-sidebar.blade.php
@@ -1,11 +1,11 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    {{-- <a href="index3.html" class="brand-link">
         <img src="{{ url('public/behin/behin-dist/dist/img/avatar5.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">پنل مدیریت</span>
-    </a>
+    </a> --}}
 
     <div class="sidebar" style="direction: ltr">
         <div style="direction: rtl">
