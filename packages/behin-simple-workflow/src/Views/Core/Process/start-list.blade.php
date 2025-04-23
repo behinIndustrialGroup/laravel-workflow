4dd22b7893c5be13408e996a27cdf9913cd08277
behinIndustrialGroup <info@behin.energy>
2025-04-23 12:18:30 +0330

    Update start-list.blade.php

--- packages/behin-simple-workflow/src/Views/Core/Process/start-list.blade.php
+++ packages/behin-simple-workflow/src/Views/Core/Process/start-list.blade.php
@@ -14,7 +14,7 @@
                 @foreach ($processes as $process)
                     <a href="{{ route('simpleWorkflow.process.start', [
                         'taskId' => $process->task->id,
-                        'inDraft' => false,
+                        'inDraft' => 0,
                         'force' => 0,
                         'redirect' => true,
                         ]) }}"
