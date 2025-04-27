1303158fe4fc2157ab1353a5e10c64b13d19a046
behinIndustrialGroup <info@behin.energy>
2025-04-27 13:26:38 +0330

    Update expired-tasks.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/Summary/process/partial/expired-tasks.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/Summary/process/partial/expired-tasks.blade.php
@@ -17,7 +17,10 @@
                     <tr>
                         <th>{{ trans('fields.Task Name') }}</th>
                         <th>{{ trans('fields.Case Name') }}</th>
+                        <th>{{ trans('fields.Case Number') }}</th>
                         <th>{{ trans('fields.Actor') }}</th>
+                        <th>{{ trans('fields.Duration') }}</th>
+                        <th>{{ trans('fields.Created At') }}</th>
                         <th>{{ trans('fields.Deadline') }}</th>
                     </tr>
                 </thead>
@@ -26,7 +29,10 @@
                         <tr>
                             <td>{{ $task->task->name }}</td>
                             <td>{{ $task->case_name }}</td>
+                            <td>{{ $task->case->number }}</td>
                             <td>{{ getUserInfo($task->actor)->name }}</td>
+                            <td>{{ $task->task->duration }}</td>
+                            <td dir="ltr">{{ toJalali($task->created_at) }}</td>
                             <td>{!! $task->time_status !!}</td>
                         </tr>
                     @endforeach
