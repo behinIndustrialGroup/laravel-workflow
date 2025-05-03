0c970e8aa8da5556af9a156b6d6617e95e9b2afb
behinIndustrialGroup <info@behin.energy>
2025-05-03 20:11:23 +0330

    Update fin-reports.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/Summary/process/partial/fin-reports.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/Summary/process/partial/fin-reports.blade.php
@@ -6,38 +6,48 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h3>{{ trans('fields.Start Process') }}</h3>
-            <hr>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="list-group">
-                @if (auth()->user()->access('منو >>گزارشات کارتابل>>مالی'))
-                    <a href="{{ route('simpleWorkflowReport.fin.totalCost') }}"
-                        class="list-group-item list-group-item-action">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">پرسنل</h5>
-                            <small class="btn btn-sm btn-primary">مشاهده</small>
-                        </div>
-                        <p class="mb-1"></p>
-                        <small>عملکرد مالی پرسنل</small>
-                    </a>
-                @endif
-                @if (auth()->user()->access('گزارش کل تعیین هزینه ها و دریافت هزینه ها'))
+    <div class="row col-sm-12">
+        <div class="card col-sm-12">
+            <div class="card-header">گزارش‌های مالی</div>
 
-                    <a href="{{ route('simpleWorkflowReport.fin.allPayments') }}"
-                        class="list-group-item list-group-item-action">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">هزینه ها</h5>
-                            <small class="btn btn-sm btn-primary">مشاهده</small>
-                        </div>
-                        <p class="mb-1"></p>
-                        <small>گزارش کامل تعیین هزینه ها و دریافت هزینه ها</small>
-                    </a>
-                @endif
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="d-none">شناسه</th>
+                                <th>عنوان گزارش</th>
+                                <th class="d-none">توضیحات</th>
+                                <th>عملیات</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if (auth()->user()->access('منو >>گزارشات کارتابل>>مالی'))
+                                <tr>
+                                    <td class="d-none"></td>
+                                    <td>پرسنل</td>
+                                    <td class="d-none">عملکرد مالی پرسنل</td>
+                                    <td>
+                                        <a href="{{ route('simpleWorkflowReport.fin.totalCost') }}"
+                                            class="btn btn-primary btn-sm">مشاهده گزارش</a>
+                                    </td>
+                                </tr>
+                            @endif
+                            @if (auth()->user()->access('گزارش کل تعیین هزینه ها و دریافت هزینه ها'))
+
+                                <tr>
+                                    <td class="d-none"></td>
+                                    <td>هزینه ها</td>
+                                    <td class="d-none">گزارش کامل تعیین هزینه ها و دریافت هزینه ها</td>
+                                    <td>
+                                        <a href="{{ route('simpleWorkflowReport.fin.allPayments') }}"
+                                            class="btn btn-primary btn-sm">مشاهده گزارش</a>
+                                    </td>
+                                </tr>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
