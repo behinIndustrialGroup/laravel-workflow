e741dba6d6d35fa8cddf8d16783997b0f43ea066
behinIndustrialGroup <info@behin.energy>
2025-05-15 15:47:30 +0330

    Update index.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/ExternalInternal/index.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/ExternalInternal/index.blade.php
@@ -13,8 +13,8 @@
                         <tr>
                             <th>{{ trans('fields.Case Number') }}</th>
                             <th>{{ trans('fields.Customer') }}</th>
-                            <th>{{ trans('fields.Last Status') }}</th>
                             <th>{{ trans('fields.Creator') }}</th>
+                            <th>{{ trans('fields.Last Status') }}</th>
                             <th>{{ trans('fields.Created At') }}</th>
                         </tr>
                     </thead>
@@ -30,13 +30,13 @@
                                     {!! $case->history !!}
                                 </td>
                                 <td>{{ $case->getVariable('customer_workshop_or_ceo_name') }}</td>
+                                <td>{{ getUserInfo($case->creator)->name }}</td>
                                 <td>
                                     @foreach ($case->whereIs() as $inbox)
                                         {!! $inbox->task->styled_name !!} <span
                                             class="text-muted">({{ getUserInfo($inbox->actor)->name }})</span> <br>
                                     @endforeach
                                 </td>
-                                <td>{{ getUserInfo($case->creator)->name }}</td>
                                 <td dir="ltr">{{ toJalali($case->created_at)->format('Y-m-d H:i') }}</td>
                             </tr>
                         @endforeach
