7faeb6f12c7f61b56e774ee6006d65aacfa9d252
behinIndustrialGroup <info@behin.energy>
2025-05-29 10:34:12 +0330

    Update web.php

--- packages/behin-simple-workflow-report/src/Routes/web.php
+++ packages/behin-simple-workflow-report/src/Routes/web.php
@@ -65,6 +65,6 @@ Route::name('simpleWorkflowReport.')->prefix('workflow-report')->middleware(['we
 
     Route::resource('mapa-center', MapaCenterController::class);
     Route::post('mapa-center/exclude-device/{mapa_center}', [MapaCenterController::class, 'excludeDevice'])->name('mapa-center.exclude-device');
-    Route::get('mapa-center-archive', [MapaCenterController::class, 'archive'])->name('mapa-center.archive');
+    Route::get('mapa-center-archive', [MapaCenterController::class, 'archive'])->name('mapa-center-archive');
 
 });
