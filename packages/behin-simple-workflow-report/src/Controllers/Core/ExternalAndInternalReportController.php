cc1d774c23d5c5d2b333e9a91dd453970b8dec1d
behinIndustrialGroup <info@behin.energy>
2025-07-09 08:45:59 +0330

    Update ExternalAndInternalReportController.php

--- packages/behin-simple-workflow-report/src/Controllers/Core/ExternalAndInternalReportController.php
+++ packages/behin-simple-workflow-report/src/Controllers/Core/ExternalAndInternalReportController.php
@@ -42,7 +42,7 @@ class ExternalAndInternalReportController extends Controller
             ->get()
             ->filter(function ($case) {
                 $whereIsResult = $case->whereIs();
-                return !($whereIsResult[0]?->archive == 'yes');
+                return !($whereIsResult->first()?->task->type == 'end');
             });
         return view('SimpleWorkflowReportView::Core.ExternalInternal.index', compact('cases'));
     }
