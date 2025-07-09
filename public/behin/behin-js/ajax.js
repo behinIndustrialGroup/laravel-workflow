a898f101a26253dda169158c8c26c990339d2d20
behinIndustrialGroup <info@behin.energy>
2025-07-09 12:57:38 +0330

    Update ajax.js

--- public/behin/behin-js/ajax.js
+++ public/behin/behin-js/ajax.js
@@ -160,7 +160,7 @@ function send_ajax_get_request_with_confirm(url, callback, message = "Are you su
 }
 
 function runScript(scriptId, data,callback){
-    url = "/laravel-workflow/workflow/scripts/" + scriptId + "/run";
+    url = appUrl + "workflow/scripts/" + scriptId + "/run";
     return send_ajax_formdata_request(
         url,
         data,
