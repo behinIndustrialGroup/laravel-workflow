bbe4200b67a492b21fb4d262110f03b6248cd457
behinIndustrialGroup <info@behin.energy>
2025-07-21 17:37:14 +0330

    حذف سشن های فعال کاربر

--- packages/behin-user-roles/src/Views/user/edit.blade.php
+++ packages/behin-user-roles/src/Views/user/edit.blade.php
@@ -19,6 +19,7 @@
         <div class="card p-2">
             <a href="{{ route('user.all', 'all') }}" class="btn btn-primary">بازگشت به لیست کاربران</a>
             <div class="row mb-3">
+                
                 <form action="{{ route('user.update', $user->id) }}" method="POST" class="row col-12 form-horizontal">
                     @csrf
                     @method('PUT')
@@ -134,98 +135,8 @@
             </form>
         </div>
     </div>
-    {{-- <div class="row">
-        <div class="col-sm-12">
-            <a href="all" class="btn btn-info">Back To List</a>
-        </div>
-        <div class="col-sm-6">
-            <div class="box">
-                <div class="box-header">
-
-                </div>
-
-                <div class="box-body">
-                    <table class="table">
-                        <tr>
-                            <th>شناسه</th>
-                            <td>{{ $user->id }}</td>
-                        </tr>
-                        <tr>
-                            <th>نام</th>
-                            <td>{{ $user->display_name }}</td>
-                        </tr>
-                        <tr>
-                            <th>نام کاربری</th>
-                            <td>{{ $user->name }}</td>
-                        </tr>
-                        <tr>
-                            <th>ایمیل</th>
-                            <td>{{ $user->email }}</td>
-                        </tr>
-                        <tr>
-                            <form method="post" action="{{ route('change-user-ip', ['id' => $user->id]) }}">
-                                @csrf
-                                <td>valid ip:<input type="text" name="valid_ip" id=""
-                                        value="{{ $user->valid_ip }}"></td>
-                                <td><input type="submit" value="ثبت ip" name="" id=""></td>
-                            </form>
-                        </tr>
-                        <tr>
-                            <form method="post" action="{{ route('change-pm-username', ['id' => $user->id]) }}">
-                                @csrf
-                                <td>نام کاربری Process Maker<input type="text" name="pm_username" id=""
-                                        value="{{ $user->pm_username }}"></td>
-                                <td><input type="submit" value="تغییر نام کاربری PM" name="" id=""></td>
-                            </form>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <button class="btn btn-danger"
-                                    onclick="create_pm_user()">{{ __('Create PM User') }}</button>
-                            </td>
-                        </tr>
-                        <tr>
-
-                        </tr>
-                    </table>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="box-body">
-                    <form method="post" action="{{ $user->id }}/changeShowInReport">
-                        @csrf
-                        <input type="checkbox" name="showInReport" class="" <?php if ($user->showInReport == 1) {
-                            echo 'checked';
-                        } ?>>نمایش در گزارشها
-                        <input type="submit" class="btn btn-success" value="ثبت">
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="box">
-                <div class="box-header">
-                    <h3>دسترسی ها</h3>
-                </div>
-
-                <div class="box-body">
-                    <button class="" id="check_all">انتخاب همه</button>
-                    <form class="form-horizontal" method="post" action="" id="role-table">
-                        @csrf
-                        <input type="text" name="user_id" id="" value="{{ $user->id }}">
-                        <select name="role_id" id="">
-                            @foreach ($roles as $role)
-                                <option value="{{ $role->id }}"
-                                    @if ($user->role_id == $role->id) {{ 'selected' }} @endif>{{ $role->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </form>
-
-                    <button onclick="change_role()">change role</button>
-                </div>
-            </div>
-        </div>
-    </div> --}}
+    <form action="{{ route('user.invalidateSessions', $user->id) }}" method="POST" onsubmit="return confirm('آیا مطمئن هستید که می‌خواهید کاربر را از همه دستگاه‌ها خارج کنید؟');">
+        @csrf
+        <button type="submit" class="btn btn-danger">خروج از همه دستگاه‌ها</button>
+    </form>
 @endsection
