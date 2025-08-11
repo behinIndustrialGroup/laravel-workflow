18338cff351b964bb8e31df36a51b4ffa0f99c2e
behinIndustrialGroup <info@behin.energy>
2025-08-11 10:22:32 +0330

    آپدیت

--- packages/behin-simple-workflow/src/Routes/web.php
+++ packages/behin-simple-workflow/src/Routes/web.php
@@ -23,6 +23,10 @@ Route::name('simpleWorkflow.')->prefix('workflow')->middleware(['web', 'auth'])-
         Route::get('start-list', [ ProcessController::class, 'startListView' ])->name('startListView');
         Route::get('start/{taskId}/{force?}/{redirect?}/{inDraft}', [ ProcessController::class, 'start' ])->name('start');
         Route::get('check-error/{processId}', [ ProcessController::class, 'processHasError' ])->name('processHasError');
+        Route::get('{processId}/export-view', [ ProcessController::class, 'exportView' ])->name('exportView');
+        Route::get('import-view', [ ProcessController::class, 'importView' ])->name('importView');
+        Route::get('{processId}/export', [ ProcessController::class, 'export' ])->name('export');
+        Route::post('import', [ ProcessController::class, 'import' ])->name('import');
     });
 
     Route::name('task.')->prefix('task')->group(function(){
@@ -54,11 +58,14 @@ Route::name('simpleWorkflow.')->prefix('workflow')->middleware(['web', 'auth'])-
     Route::resource('scripts', ScriptController::class);
     Route::post('scripts/{id}/test', [ ScriptController::class, 'test' ])->name('scripts.test');
     Route::any('scripts/{id}/run', [ ScriptController::class, 'runFromView' ])->name('scripts.run');
+    Route::post('/scripts/autocomplete', [ScriptController::class, 'autocomplete'])->name('scripts.autocomplete');
+
 
     Route::resource('conditions', ConditionController::class);
     Route::post('conditions/{id}/test', [ ConditionController::class, 'runConditionForTest' ])->name('conditions.test');
     Route::resource('task-actors', TaskActorController::class);
     Route::resource('fields', FieldController::class);
+    Route::get('fields/{field}/copy', [FieldController::class, 'copy'])->name('fields.copy');
 
     Route::name('inbox.')->prefix('inbox')->group(function(){
         Route::get('', [ InboxController::class, 'index' ])->name('index');
