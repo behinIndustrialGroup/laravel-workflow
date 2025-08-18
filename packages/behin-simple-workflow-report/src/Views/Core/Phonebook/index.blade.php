2c8e0291029777030f3bdb2bfe738a36b7b13478
behinIndustrialGroup <info@behin.energy>
2025-08-18 09:59:00 +0330

    Update index.blade.php

--- packages/behin-simple-workflow-report/src/Views/Core/Phonebook/index.blade.php
+++ packages/behin-simple-workflow-report/src/Views/Core/Phonebook/index.blade.php
@@ -6,7 +6,8 @@
     <div class="container">
         <div class="card">
             <div class="card-header bg-success">دفتر تلفن
-                <a href="{{ route('simpleWorkflowReport.phonebook.create') }}" class="btn btn-light btn-sm float-end" style="color: black !important">افزودن</a>
+                <a href="{{ route('simpleWorkflowReport.phonebook.create') }}" class="btn btn-light btn-sm float-end"
+                    style="color: black !important">افزودن</a>
             </div>
             <div class="card-body table-responsive">
                 <table class="table" id="phonebook">
@@ -20,15 +21,18 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($customers as $customer)
+                        @foreach ($customers as $customer)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
                                 <td>{{ $customer->name }}</td>
                                 <td>{{ $customer->mobile }}</td>
                                 <td>{{ $customer->address }}</td>
                                 <td>
-                                    <a href="{{ route('simpleWorkflowReport.phonebook.edit', $customer->id) }}" class="btn btn-primary btn-sm">ویرایش</a>
-                                    <form method="POST" action="{{ route('simpleWorkflowReport.phonebook.destroy', $customer->id) }}" class="d-inline">
+                                    <a href="{{ route('simpleWorkflowReport.phonebook.edit', $customer->id) }}"
+                                        class="btn btn-primary btn-sm">ویرایش</a>
+                                    <form method="POST"
+                                        action="{{ route('simpleWorkflowReport.phonebook.destroy', $customer->id) }}"
+                                        class="d-inline">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger btn-sm">حذف</button>
@@ -45,22 +49,29 @@
 
 @section('script')
     <script>
-        $('#phonebook').DataTable({
-            "dom": 'Bfrtip',
-            @if(auth()->user()->access('خروجی اکسل دفتر تلفن'))
-            "buttons": [{
-                "extend": 'excelHtml5',
-                "text": "خروجی اکسل",
-                "title": "دفتر تلفن",
-                "className": "btn btn-success btn-sm",
-                "exportOptions": {
-                    "columns": ':visible'
+        @if (auth()->user()->access('خروجی اکسل دفتر تلفن'))
+            $('#phonebook').DataTable({
+                "dom": 'Bfrtip',
+                "buttons": [{
+                    "extend": 'excelHtml5',
+                    "text": "خروجی اکسل",
+                    "title": "دفتر تلفن",
+                    "className": "btn btn-success btn-sm",
+                    "exportOptions": {
+                        "columns": ':visible'
+                    }
+                }],
+                "language": {
+                    "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Persian.json"
                 }
-            }],
-            @endif
-            "language": {
-                "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Persian.json"
-            }
-        });
+            });
+        @else
+            $('#phonebook').DataTable({
+                "dom": 'Bfrtip',
+                "language": {
+                    "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Persian.json"
+                }
+            });
+        @endif
     </script>
 @endsection
