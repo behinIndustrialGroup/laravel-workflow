a683a0a236b091b9f133d7e6fd9af0eadf0fd100
behinIndustrialGroup <info@behin.energy>
2025-08-22 22:40:28 +0330

    Update ChequeReportController.php

--- packages/behin-simple-workflow-report/src/Controllers/Core/ChequeReportController.php
+++ packages/behin-simple-workflow-report/src/Controllers/Core/ChequeReportController.php
@@ -3,16 +3,8 @@
 namespace Behin\SimpleWorkflowReport\Controllers\Core;
 
 use App\Http\Controllers\Controller;
-use Behin\SimpleWorkflow\Controllers\Core\CaseController;
-use Behin\SimpleWorkflow\Controllers\Core\FormController;
-use Behin\SimpleWorkflow\Controllers\Core\InboxController;
-use Behin\SimpleWorkflow\Controllers\Core\ProcessController;
-use Behin\SimpleWorkflow\Controllers\Core\TaskController;
-use Behin\SimpleWorkflow\Controllers\Core\VariableController;
-use Behin\SimpleWorkflow\Models\Core\Process;
-use Behin\SimpleWorkflow\Models\Core\TaskActor;
-use Behin\SimpleWorkflow\Models\Core\Variable;
 use Behin\SimpleWorkflow\Models\Entities\Financials;
+use Behin\SimpleWorkflow\Models\Entities\OnCreditPayment;
 use Behin\SimpleWorkflowReport\Helper\ReportHelper;
 use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
