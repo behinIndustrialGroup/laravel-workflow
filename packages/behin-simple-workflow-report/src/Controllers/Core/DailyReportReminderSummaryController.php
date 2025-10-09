b5e46bb519d07ce9a0ae17947e74619beec67a6f
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2025-10-09 09:24:29 +0330

    Add Excel export to daily report reminder summary

--- packages/behin-simple-workflow-report/src/Controllers/Core/DailyReportReminderSummaryController.php
+++ packages/behin-simple-workflow-report/src/Controllers/Core/DailyReportReminderSummaryController.php
@@ -3,6 +3,7 @@
 namespace Behin\SimpleWorkflowReport\Controllers\Core;
 
 use App\Http\Controllers\Controller;
+use Behin\SimpleWorkflowReport\Exports\DailyReportReminderExport;
 use Behin\SimpleWorkflowReport\Models\DailyReportReminderLog;
 use Behin\SimpleWorkflowReport\Models\RewardPenalty;
 use Behin\SimpleWorkflowReport\Services\DailyReportReminderService;
@@ -11,7 +12,9 @@ use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Facades\Excel;
 use Morilog\Jalali\Jalalian;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DailyReportReminderSummaryController extends Controller
 {
@@ -20,6 +23,47 @@ class DailyReportReminderSummaryController extends Controller
     }
 
     public function index(Request $request): View
+    {
+        $data = $this->buildSummaryData($request);
+
+        return view('SimpleWorkflowReportView::Core.DailyReportReminder.index', [
+            'users' => $data['users'],
+            'allUsers' => $data['allUsers'],
+            'fromDate' => $data['fromDate'],
+            'toDate' => $data['toDate'],
+            'selectedUserId' => $data['selectedUserId'],
+            'rewardDetailsByUser' => $data['rewardDetailsByUser'],
+            'penaltyDetailsByUser' => $data['penaltyDetailsByUser'],
+            'filterQuery' => $data['filterQuery'],
+        ]);
+    }
+
+    public function export(Request $request): BinaryFileResponse
+    {
+        $data = $this->buildSummaryData($request);
+
+        $fileName = sprintf('dailyReportReminder_%s.xlsx', now()->format('Y-m-d'));
+
+        return Excel::download(
+            new DailyReportReminderExport($data['exportRows'], $data['rewardDetailsByUser'], $data['penaltyDetailsByUser']),
+            $fileName
+        );
+    }
+
+    /**
+     * @return array{
+     *     users: Collection<int, User>,
+     *     allUsers: Collection<int, User>,
+     *     fromDate: string,
+     *     toDate: string,
+     *     selectedUserId: mixed,
+     *     rewardDetailsByUser: array<int|string, array<int, array<string, mixed>>>,
+     *     penaltyDetailsByUser: array<int|string, array<int, array<string, mixed>>>,
+     *     filterQuery: array<string, mixed>,
+     *     exportRows: Collection<int, array<string, mixed>>
+     * }
+     */
+    private function buildSummaryData(Request $request): array
     {
         $defaultFrom = Jalalian::now()->format('Y-m-d');
         $fromInput = convertPersianToEnglish($request->input('from_date', $defaultFrom));
@@ -125,7 +169,27 @@ class DailyReportReminderSummaryController extends Controller
         $fromDate = Jalalian::fromCarbon($from)->format('Y-m-d');
         $toDate = Jalalian::fromCarbon($to)->format('Y-m-d');
 
-        return view('SimpleWorkflowReportView::Core.DailyReportReminder.index', [
+        $exportRows = $users->values()->map(function (User $user, int $index) {
+            return [
+                'row_number' => $index + 1,
+                'user_id' => (int) $user->id,
+                'number' => $user->number,
+                'name' => $user->name,
+                'reminder_count' => $user->reminder_count ?? 0,
+                'missing_report_count' => $user->missing_report_count ?? 0,
+            ];
+        });
+
+        $filterQuery = [
+            'from_date' => $fromDate,
+            'to_date' => $toDate,
+        ];
+
+        if ($request->filled('user_id')) {
+            $filterQuery['user_id'] = $request->input('user_id');
+        }
+
+        return [
             'users' => $users,
             'allUsers' => $allUsers,
             'fromDate' => $fromDate,
@@ -133,6 +197,8 @@ class DailyReportReminderSummaryController extends Controller
             'selectedUserId' => $request->input('user_id'),
             'rewardDetailsByUser' => $rewardDetails,
             'penaltyDetailsByUser' => $penaltyDetails,
-        ]);
+            'filterQuery' => $filterQuery,
+            'exportRows' => $exportRows,
+        ];
     }
 }
