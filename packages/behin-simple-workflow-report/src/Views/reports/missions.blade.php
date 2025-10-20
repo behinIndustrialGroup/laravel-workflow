75a686c1c99909bd1ea83e94f30b1cc4cac75f1f
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2025-10-20 17:46:58 +0330

    Add missions report view

---
+++ packages/behin-simple-workflow-report/src/Views/reports/missions.blade.php
@@ -0,0 +1,103 @@
+@extends('behin-layouts.app')
+
+@section('title', 'گزارش مأموریت‌ها')
+@php
+    use Morilog\Jalali\Jalalian;
+@endphp
+
+@section('content')
+    <div class="card mb-3">
+        <div class="card-header">
+            <form class="row" method="GET">
+                <div class="col-md-3">
+                    <select name="month" id="month-filter" class="form-select form-control">
+                        @foreach ($monthOptions as $option)
+                            <option value="{{ $option['value'] }}" data-from="{{ $option['from'] }}" data-to="{{ $option['to'] }}" {{ $selectedMonth === $option['value'] ? 'selected' : '' }}>
+                                {{ $option['label'] }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <input type="text" name="from_date" class="form-control persian-date" value="{{ $fromValue }}" placeholder="از تاریخ">
+                </div>
+                <div class="col-md-3">
+                    <input type="text" name="to_date" class="form-control persian-date" value="{{ $toValue }}" placeholder="تا تاریخ">
+                </div>
+                <div class="col-md-3">
+                    <button class="btn btn-warning" type="submit">فیلتر</button>
+                </div>
+            </form>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="missions-table">
+                    <thead>
+                        <tr>
+                            <th>عنوان مأموریت</th>
+                            <th>تاریخ شروع</th>
+                            <th>تاریخ پایان</th>
+                            <th>ایجادکننده</th>
+                            <th>مدت (ساعت)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($missions as $mission)
+                            <tr>
+                                <td>{{ $mission->title }}</td>
+                                <td dir="ltr">
+                                    {{ $mission->start_datetime_carbon ? toJalali($mission->start_datetime_carbon)->format('Y-m-d H:i') : '' }}
+                                </td>
+                                <td dir="ltr">
+                                    {{ $mission->end_datetime_carbon ? toJalali($mission->end_datetime_carbon)->format('Y-m-d H:i') : '' }}
+                                </td>
+                                <td>{{ $mission->created_by }}</td>
+                                <td dir="ltr">
+                                    {{ $mission->duration_hours !== null ? number_format($mission->duration_hours, 2) : '-' }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        $('#missions-table').DataTable({
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.13.6/i18n/fa.json'
+            },
+            order: [[1, 'desc']],
+            pageLength: 25
+        });
+
+        $(function() {
+            const monthFilter = document.getElementById('month-filter');
+            const fromInput = document.querySelector('input[name="from_date"]');
+            const toInput = document.querySelector('input[name="to_date"]');
+
+            function applySelectedMonth() {
+                if (!monthFilter) {
+                    return;
+                }
+                const selectedOption = monthFilter.options[monthFilter.selectedIndex];
+                if (!selectedOption) {
+                    return;
+                }
+                if (fromInput && selectedOption.dataset.from) {
+                    fromInput.value = selectedOption.dataset.from;
+                }
+                if (toInput && selectedOption.dataset.to) {
+                    toInput.value = selectedOption.dataset.to;
+                }
+            }
+
+            if (monthFilter) {
+                monthFilter.addEventListener('change', applySelectedMonth);
+            }
+        });
+    </script>
+@endsection
