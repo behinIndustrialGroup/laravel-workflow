409a0e1c6926f74612a58d4f1d25acdf1dadf40e
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2025-11-06 14:14:37 +0330

    Ensure goods-in report auto applies default range

--- packages/behin-simple-workflow-report/src/Controllers/Core/GoodsInReportController.php
+++ packages/behin-simple-workflow-report/src/Controllers/Core/GoodsInReportController.php
@@ -126,6 +126,37 @@ class GoodsInReportController extends Controller
             $dateColumns = $dateColumns->prepend($selectedDateColumn);
         }
 
+        $filters['date_column'] = $selectedDateColumn;
+
+        $shouldApplyDefaultDateRange =
+            $filters['from_date'] === ''
+            && $filters['from_date_alt'] === ''
+            && $filters['to_date'] === ''
+            && $filters['to_date_alt'] === '';
+
+        if ($shouldApplyDefaultDateRange && $selectedDateColumn) {
+            $defaultTo = Carbon::now($timezone)->endOfDay();
+            $defaultFrom = (clone $defaultTo)->subDays(2)->startOfDay();
+
+            $redirectQuery = array_merge($request->query(), [
+                'from_date' => Jalalian::fromCarbon($defaultFrom)->format('Y-m-d'),
+                'from_date_alt' => (string) $defaultFrom->valueOf(),
+                'to_date' => Jalalian::fromCarbon($defaultTo)->format('Y-m-d'),
+                'to_date_alt' => (string) $defaultTo->valueOf(),
+                'date_column' => $selectedDateColumn,
+            ]);
+
+            if (! isset($redirectQuery['per_page'])) {
+                $redirectQuery['per_page'] = $filters['per_page'];
+            }
+
+            if (! isset($redirectQuery['sort_direction'])) {
+                $redirectQuery['sort_direction'] = $filters['sort_direction'];
+            }
+
+            return redirect()->route('simpleWorkflowReport.goods-in.index', $redirectQuery);
+        }
+
         $query = DB::table($tableName);
         $validationErrors = [];
 
